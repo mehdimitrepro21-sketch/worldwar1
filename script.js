388fa49045dab1fda3edb29a258dd6436e94de15
mehdimitrepro21-sketch <mehdimitrepro21@gmail.com>
2026-07-29 06:15:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("learn");
+
+button.addEventListener("click", function () {
+    alert("World War I lasted from 1914 to 1918 and involved many major world powers.");
+});
